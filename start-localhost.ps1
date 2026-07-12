d76af55bc9039a49fae364beec59914a6c3ac05a
melisarama <melisarama8@gmail.com>
2026-07-12 07:52:53 +0200

    Link uploaded PIA document to pupil portfolio

--- start-localhost.ps1
+++ start-localhost.ps1
@@ -1,6 +1,7 @@
 $root = Split-Path -Parent $MyInvocation.MyCommand.Path
 $port = 8080
 $htmlPath = Join-Path $root 'index.html'
+$piaPath = Join-Path $root 'plani_individual_i_arsimit.pdf'
 $listener = [System.Net.Sockets.TcpListener]::new([System.Net.IPAddress]::Any, $port)
 
 try {
@@ -11,9 +12,17 @@ try {
         try {
             $stream = $client.GetStream()
             $buffer = New-Object byte[] 4096
-            $stream.Read($buffer, 0, $buffer.Length) | Out-Null
-            $body = [System.IO.File]::ReadAllBytes($htmlPath)
-            $header = "HTTP/1.1 200 OK`r`nContent-Type: text/html; charset=utf-8`r`nContent-Length: $($body.Length)`r`nConnection: close`r`n`r`n"
+            $read = $stream.Read($buffer, 0, $buffer.Length)
+            $request = [System.Text.Encoding]::ASCII.GetString($buffer, 0, $read)
+            $requestPath = ($request -split "`r?`n")[0].Split(' ')[1]
+            if ($requestPath -like '/plani_individual_i_arsimit.pdf*' -and (Test-Path -LiteralPath $piaPath)) {
+                $body = [System.IO.File]::ReadAllBytes($piaPath)
+                $contentType = 'application/pdf'
+            } else {
+                $body = [System.IO.File]::ReadAllBytes($htmlPath)
+                $contentType = 'text/html; charset=utf-8'
+            }
+            $header = "HTTP/1.1 200 OK`r`nContent-Type: $contentType`r`nContent-Length: $($body.Length)`r`nConnection: close`r`n`r`n"
             $headerBytes = [System.Text.Encoding]::ASCII.GetBytes($header)
             $stream.Write($headerBytes, 0, $headerBytes.Length)
             $stream.Write($body, 0, $body.Length)
